1e7a7b7e92a3b1fb4245074435f86a73bcb036b1
Chkalov Evgeny <e.chkalov@itransition.com>
2019-02-25 10:20:21 +0300

    add profile card component

--- iqs-ui-web/src/modules/profile/components/profile-card/profile-card.js
+++ iqs-ui-web/src/modules/profile/components/profile-card/profile-card.js
@@ -4,11 +4,7 @@ import http from '../../../../services/axios-http';
 import { STORAGE_KEYS } from '../../../../services/storage';
 
 export default () => {
-    const [personData, setPersonData] = useState({
-        name: '',
-        age: 0,
-        role: ''
-    });
+    const [personData, setPersonData] = useState({ department: {}, grade: {} });
 
     useEffect(() => {
         http.get(`${http.getApiUri()}/user/${localStorage.getItem(STORAGE_KEYS.CURRENT_USER)}`).then(response => {
@@ -17,9 +13,37 @@ export default () => {
     }, []);
 
     return (
-        <div>
-            This is your profile:
-            {JSON.stringify(personData)}
+        <div className="profile-card">
+            <div className="person-general">
+                <div className="person-general__photo">photo</div>
+                <div className="person-general__name">{personData.name}</div>
+                <div className="person-general__department">{personData.department.name}</div>
+            </div>
+            <div className="grade-panel">
+                <div className="grade-panel__overview">
+                    <div className="grade-panel__chart">
+                        <div className="grade-panel__range">
+                            <div className="grade-panel__grade-item obtained">D0</div>
+                            <div className="grade-panel__grade-item obtained">D1</div>
+                            <div className="grade-panel__grade-item obtained">D2</div>
+                            <div className="grade-panel__grade-item">D3</div>
+                            <div className="grade-panel__grade-item">D4</div>
+                            <div className="grade-panel__grade-item">D5</div>
+                            <div className="grade-panel__grade-item">D6</div>
+                            <div className="grade-panel__grade-item">D7</div>
+                            <div className="grade-panel__grade-item">D8</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="grade-requirements">
+                    <ul>
+                        <li>req</li>
+                        <li>req</li>
+                        <li>req</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
